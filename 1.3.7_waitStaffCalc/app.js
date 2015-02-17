62c868b7181281fcd58fe7110f594d1c4117d7f8
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2015-02-17 09:37:25 -0800

    Add watchers for mealPrice, taxRate, tipRate

--- 1.3.7_waitStaffCalc/app.js
+++ 1.3.7_waitStaffCalc/app.js
@@ -5,9 +5,48 @@ app.controller("waitCtrl", function($scope){
 		"example": [10,20,30]
 	};
 
+	$scope.thisMeal = {
+		"mealPrice": null,
+		"taxRate": null,
+		"tipRate": null,
+		"subTotal": null,
+		"tipTotal": null,
+		"totalTotal": null
+	};
+
 	$scope.addMeal = function(){
 		var customer = "customer" + (Object.keys($scope.meals).length+1);
 		$scope.meals[customer] = [$scope.mealPrice, $scope.taxRate, $scope.tipRate];
 	};
+
+	$scope.$watch('mealPrice', function(newVal, oldVal){
+		$scope.thisMeal.mealPrice = newVal;
+
+		// update totalTotal if form is already filled out
+		if ($scope.thisMeal.taxRate !== null){
+			$scope.thisMeal.subTotal = $scope.thisMeal.mealPrice + ($scope.thisMeal.mealPrice * ($scope.thisMeal.taxRate/100));
+			$scope.thisMeal.tipTotal = $scope.thisMeal.mealPrice * ($scope.thisMeal.tipRate/100);
+			$scope.thisMeal.totalTotal = $scope.thisMeal.subTotal + $scope.thisMeal.tipTotal;
+		}
+	});
+
+	$scope.$watch('taxRate', function(newVal, oldVal){
+		$scope.thisMeal.taxRate = newVal;
+		$scope.thisMeal.subTotal = $scope.thisMeal.mealPrice + ($scope.thisMeal.mealPrice * ($scope.thisMeal.taxRate/100));
+
+		// update totalTotal if form is already filled out
+		if ($scope.thisMeal.subTotal && $scope.thisMeal.tipTotal){
+			$scope.thisMeal.totalTotal = $scope.thisMeal.subTotal + $scope.thisMeal.tipTotal;
+		}
+	});
+
+	$scope.$watch('tipRate', function(newVal, oldVal){
+		$scope.thisMeal.tipRate = newVal;
+		$scope.thisMeal.tipTotal = $scope.thisMeal.mealPrice * ($scope.thisMeal.tipRate/100);
+		$scope.thisMeal.totalTotal = $scope.thisMeal.subTotal + $scope.thisMeal.tipTotal;
+	});
+
+	
+	
 
 });
